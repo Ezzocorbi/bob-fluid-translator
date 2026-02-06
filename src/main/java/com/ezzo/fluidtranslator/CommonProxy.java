40a7813d2a21ff769dbd9c2a324cde1a0c6e7a52
Ezzocorbi <36800339+Ezzocorbi@users.noreply.github.com>
2026-02-06 17:19:16 +0100

    Remove unused method from proxies

--- src/main/java/com/ezzo/fluidtranslator/CommonProxy.java
+++ src/main/java/com/ezzo/fluidtranslator/CommonProxy.java
@@ -1,14 +1,10 @@
 package com.ezzo.fluidtranslator;
 
-import net.minecraftforge.fluids.Fluid;
-
 public class CommonProxy {
 
     public CommonProxy() { }
 
     public void registerEvents() { }
-
-    public void registerRenders() { }
 
     public void registerTanslations() { }
 }
